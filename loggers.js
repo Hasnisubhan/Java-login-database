12b89aca8d54363e317d23633544f01e1630f5db
Hasni Subhan <hasnisubhan@gmail.com>
2023-08-15 13:06:58 +0530

    Add files via upload

---
+++ loggers.js
@@ -0,0 +1,23 @@
+ 
+function auth(event) {
+      event.preventDefault();
+
+      var tel = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+
+      /*username and password data 1*/
+      if (tel === "hasni" && password === "123") {
+          window.location.replace("pages/page2.html"); return;
+      }
+
+      /*username and password data 2*/
+      if (tel === "subhan" && password === "3030"){
+          window.location.replace("pages/page1.html"); return;
+      }
+
+      else {
+          alert("Invalid username or password");
+          return;
+      }
+}
+
